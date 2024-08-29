2dcf26e04b8ae9bd853802abc4a580b37ea0196c
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-08-29 14:42:17 -0500

    Updated Greeting component in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+import React from "react";
+import Greeting from "./components/Greeting";
 function App() {
-  return <div className="app">Hello Atlas</div>;
+  // return <div className="app">Hello Atlas</div>;
+  return <div className="App">
+    <Greeting/>
+  </div>
 }
 
 export default App;
