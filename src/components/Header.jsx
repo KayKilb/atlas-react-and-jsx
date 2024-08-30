9522c45626c1f0d890d3108c3a171ecd5e8dd45a
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-08-29 20:58:41 -0500

    added header and social links

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Greeting from './Greeting';
+import SocialLinks from './SocialLinks';
+
+const Header = () => {
+    return (
+        <nav>
+            <Greeting />
+            <SocialLinks />
+        </nav>
+    );
+};
