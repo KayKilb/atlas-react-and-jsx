a69ff16cc00dc8ef9977aa61b79e6df55ef5c759
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-08-29 20:51:55 -0500

    socials added

---
+++ src/components/SocialLinks.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const SocialLinks = () => {
+    return (
+        <div className="social-links">
+            <a href="https://www.linkedin.com/in/kaylenekilbourn/" target="_blank" rel="noopener noreferrer">
+                <img src="/src/assets/linkedin.svg" alt="LinkedIn" />
+            </a>
+            <a href="https://github.com/KayKilb/atlas-react-and-jsx" target="_blank" rel="noopener noreferrer">
+                <img src="/src/assets/github.svg" alt="GitHub" />
+            </a>
+        </div>
+    );
+};
+
+export default SocialLinks;
