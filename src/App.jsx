e467d379e849d94e2d9d01b4e64d34067a91b614
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-08-29 21:14:21 -0500

    updated call back to src

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "./components/Header";
-import Greeting from "./components/Greeting";
+import Greeting from "./components/Greeting.jsx";
 import SocialLinks from "./components/SocialLinks";
 
 function App() {
