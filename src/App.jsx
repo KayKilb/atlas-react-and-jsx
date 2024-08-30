f46fd44066e456f5c29160d7d2c7bf6996f86ed2
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-08-29 21:05:02 -0500

    adding Header to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
 import React from "react";
-import Greeting from "./components/Greeting";
-import SocialLinks from "./components/SocialLinks";
 import Header from "./components/Header";
 
 function App() {
-  // return <div className="app">Hello Atlas</div>;
-  return <div className="App">
-    <Greeting />
-    <SocialLinks />
-    <Header />
-  </div>
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  );
 }
 
 export default App;
