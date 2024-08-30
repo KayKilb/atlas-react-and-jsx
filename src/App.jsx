579d4ca27005dbc1f95f3524c1b192503dce66f7
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-08-29 21:11:44 -0500

    App not rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Header from "./components/Header";
+import Greeting from "./components/Greeting";
+import SocialLinks from "./components/SocialLinks";
 
 function App() {
-  return (
-    <div className="App">
-      <Header />
-    </div>
-  );
+  // return <div className="app">Hello Atlas</div>;
+  return <div className="App">
+    <Header />
+  </div>
 }
 
 export default App;
+
