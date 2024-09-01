0189bea225cefcd4dfbbd417e8c4ad788f622d3e
KayKilb <115676011+KayKilb@users.noreply.github.com>
2024-08-31 23:09:52 -0500

    ProfilePicture updated

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -7,7 +7,7 @@ const AboutMe = () => {
             <img
                 src={profilePicture}
                 alt="Kaylene Kilbourn"
-                style={{ width: '150px', borderRadius: '50%' }}
+                style={{ width: '200px', height: '200px', borderRadius: '15px', objectFit: 'cover' }}
             />
             <p>
                 Hi, I'm Kaylene Kilbourn. I'm specializing in Full Stack Web Development and currently in my 5th and final trimester. I love working with React, Python, and HTML. After school, I plan to work in an industry that fulfills both my creative and logical sides. Outside of work and school, I enjoy reading, photography, and Pokémon GO.
